8a967ebacc23a5813862fdf339a633ddfaca301f
LXZ <Lampxiezi@163.com>
2017-11-01 18:02:14 +0800

    add

--- 1.php
+++ 1.php
@@ -1,2 +1,11 @@
 <?php
-	echo `ls -l`;
+// 提取网页关键字
+$meta = get_meta_tags('http://www.jb51.net/');
+$keywords = $meta['keywords'];
+// Split keywords
+$keywords = explode(',', $keywords );
+// Trim them
+$keywords = array_map( 'trim', $keywords );
+// Remove empty values
+$keywords = array_filter( $keywords );
+print_r( $keywords );
---
+++ phpQuery/demo.php
@@ -0,0 +1,4 @@
+<?php
+require './vendor/autoload.php';
+
+// 获取网页标题
